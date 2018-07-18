5bf8d20d00673ca5c25d9313c8b97ca596b83e4b
zhangcaochao <zhangcaochao30@gmail.com>
2018-07-18 14:14:08 +0800

    init
    第一次提交

---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,19 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
+public interface IUserService {
+
+    ServerResponse<User> login(String usename, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str,String type);
+
+    ServerResponse<String> selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username,String question,String answer);
+
+
+}
